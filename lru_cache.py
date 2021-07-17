cc3d951991c60d7f77b270fa0b677786baf2a309
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-07-17 15:38:00 +0530

    Updated

--- lru_cache.py
+++ lru_cache.py
@@ -4,12 +4,19 @@ class LRU:
     def __init__(self, capacity):
         self.cache = OrderedDict()
         self.capacity = capacity
-       
+
     def get_cache(self):
-        pass
+        return self.cache
     
     def get(self, key):
-        pass
-    
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+            
     def put(self, key, value):
-        pass
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
